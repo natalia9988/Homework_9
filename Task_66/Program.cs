e56be80fef48efe0855d0409ead2d39bad60edf0
Natalia <natalia-9988@yandex.ru>
2023-02-06 13:32:38 +0500

    Task_68

---
+++ Task_66/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 66: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+int getIntFromUser(string message)  
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+int userNumberM = getIntFromUser("Введите начальное значение М ");
+int userNumberN =getIntFromUser ("Введите конечное значение N ");
+int Sum = getSumMN( userNumberM, userNumberN);
+Console.Write(Sum);
+
+int getSumMN (int m, int n)
+{
+    return m == n? n : m + getSumMN(m + 1, n);
+}
+
---
+++ Task_68/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
+
+int getIntFromUser(string message)  
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+int userNumberM = getIntFromUser("Введите начальное значение М ");
+int userNumberN =getIntFromUser ("Введите конечное значение N ");
+int AkkermanFunction = Akkerman(userNumberM,userNumberN);
+Console.Write(AkkermanFunction);
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+
+
